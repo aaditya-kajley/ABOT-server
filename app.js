a1acb82799a75551968eeb2c65a91273ea845b0d
aaditya-kajley <110079245+aaditya-kajley@users.noreply.github.com>
2024-04-15 14:09:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-  origin: 'https://abot-client.vercel.app/', // Update this with your frontend URL
+  origin: 'https://shiny-donut-b0a149.netlify.app/', // Update this with your frontend URL
   credentials: true 
 }
 ));
